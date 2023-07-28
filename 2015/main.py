d90df44899e2df8fd17eb47b3e3eafbb3cbab055
Philémon van Helden <philemon.vanhelden@gmx.com>
2023-07-28 15:29:29 +0200

    [ADD] Day 03 part 2

--- 2015/main.py
+++ 2015/main.py
@@ -1,8 +1,6 @@
 import re
 from math import prod
 
-from tqdm import tqdm
-
 from utils import sum_tuples
 
 
@@ -46,15 +44,28 @@ def day02():
 def day03():
     with open('data/day03.txt') as file:
         text = file.read().strip('\n')
-        pos = [0, 0]
+        pos_part_1, pos_robot, pos_santa = [[0, 0]] * 3
         char_dic: dict[str, (int, int)] = {'^': (1, 0), '>': (0, 1), '<': (0, -1), 'v': (-1, 0)}
-        dic: dict[(int, int), int] = {(0, 0): 1}
-        for index, char in tqdm(enumerate(text)):
+        dic_part_1: dict[(int, int), int] = {(0, 0): 1}
+        dic_part_2: dict[(int, int), int] = {(0, 0): 1}
+        for index, char in enumerate(text):
             move = char_dic[char]
-            pos = sum_tuples(pos, move)
-            dic.setdefault(pos, 0)
-            dic[pos] += 1
-    return len(dic.values())
+
+            # Part 1
+            pos_part_1 = sum_tuples(pos_part_1, move)
+            dic_part_1.setdefault(pos_part_1, 0)
+            dic_part_1[pos_part_1] += 1
+
+            # Part 2
+            if index % 2 == 0:
+                pos_part_2 = sum_tuples(move, pos_santa)
+                pos_santa = pos_part_2
+            else:
+                pos_part_2 = sum_tuples(move, pos_robot)
+                pos_robot = pos_part_2
+            dic_part_2.setdefault(pos_part_2, 0)
+            dic_part_2[pos_part_2] += 1
+    return len(dic_part_1.values()), len(dic_part_2.values())
 
 
 def main():
