4ab7dc857d116948e3ed51143581eeee7a2ec797
Philémon van Helden <philemon.vanhelden@gmx.com>
2023-08-02 23:25:52 +0200

    [ADD] Day 04 part 2

--- 2015/main.py
+++ 2015/main.py
@@ -68,14 +68,18 @@ def day03(text: str):
 
 
 def day04(key: str):
+    res = [0, 0]
     number = 1
-    hex_hash = md5(f'{key}{number}'.encode())
     with tqdm() as pbar:
-        while not hex_hash.hexdigest().startswith('0' * 5):
+        while not res[0] or not res[1]:
             number += 1
             hex_hash = md5(f'{key}{number}'.encode())
+            if not res[0] and hex_hash.hexdigest().startswith('0' * 5):
+                res[0] = number
+            if not res[1] and hex_hash.hexdigest().startswith('0' * 6):
+                res[1] = number
             pbar.update()
-        return number, 0
+        return res[0], res[1]
 
 
 def main():
