b4f0e432fa14b7d1bea33b40a82acd5e2ef4a337
Philémon van Helden <philemon.vanhelden@gmx.com>
2023-08-15 11:35:28 +0200

    [ADD] Day 07 part 2

--- 2015/main.py
+++ 2015/main.py
@@ -131,13 +131,15 @@ def day06(instructions: list[str]):
     return grid1.count_nonzero(), grid2.sum()
 
 
-def day07(instructions: list[str]):
-    signals: dict[(str, int)] = dict()
+def apply_instructions(instructions: list[str], signals: dict[(str, int)]):
     with tqdm() as pbar:
         while len(instructions):
             line = instructions.pop(0)
             signal, wire = line.split(' -> ')
             signal = signal.split(' ')
+            if line == '1674 -> b' and 'b' in signals:
+                pbar.update()
+                continue
 
             # Check if eventual input wires already have a value assigned. If not, do this line later.
             end = False
@@ -151,13 +153,13 @@ def day07(instructions: list[str]):
 
             # If eventual input wires already have a value assigned.
             if len(signal) == 1:
-                res = signals[signal[0]] if not signal[0].isdigit() else uint16(signal[0])
+                res = signals[signal[0]] if not signal[0].isdigit() else int(signal[0])
             elif len(signal) == 2:  # NOT
                 res = ~uint16(signals[signal[1]])
             else:  # len(signal) == 3
                 left, oper, right = signal
                 left = signals[left] if not left.isdigit() else int(left)
-                right = signals[right] if not right.isdigit() else uint16(right)
+                right = signals[right] if not right.isdigit() else int(right)
                 if oper == 'AND':
                     res = left & right
                 elif oper == 'OR':
@@ -168,7 +170,14 @@ def day07(instructions: list[str]):
                     res = left >> right
             signals[wire] = res
             pbar.update()
-    return signals['a'], 0
+
+
+def day07(instructions: list[str]):
+    signals1: dict[(str, int)] = dict()
+    apply_instructions(instructions.copy(), signals1)
+    signals2: dict[(str, int)] = {'b': signals1['a']}
+    apply_instructions(instructions, signals2)
+    return signals1['a'], signals2['a']
 
 
 def main():
