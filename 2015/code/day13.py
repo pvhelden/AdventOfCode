ae812402193f6a150036ee022dcb9765206c0f27
Philémon van Helden <philemon.vanhelden@gmx.com>
2023-09-04 10:40:17 +0200

    [IMP] Day 13: Improved speed by pruning some permutations

--- 2015/code/day13.py
+++ 2015/code/day13.py
@@ -4,32 +4,50 @@ from tqdm import tqdm
 
 
 def parse_happiness_data(lines: list[str]):
-    happiness_data = {}
+    relations: dict[int, dict[int, int]] = dict()
+    persons: dict[str, int] = dict()
 
     for line in lines:
         parts = line.strip('.').split()
         person1 = parts[0]
         person2 = parts[-1]
+
+        persons.setdefault(person1, len(persons))
+        persons.setdefault(person2, len(persons))
+        relations.setdefault(persons[person1], dict())
+
         happiness = int(parts[3]) * (-1 if parts[2] == 'lose' else 1)
+        relations[persons[person1]][persons[person2]] = happiness
 
-        happiness_data.setdefault(person1, dict())
-        happiness_data[person1][person2] = happiness
+    return relations
 
-    return happiness_data
+
+def get_max_remaining(persons: list[int], highests: list[int]):
+    return sum([highests[person] for person in persons])
 
 
 def find_max_happiness(input_list: list[str]):
-    happiness_data = parse_happiness_data(input_list)
+    relations = parse_happiness_data(input_list)
+    highests = [sum(sorted(relations[person].values())[-2:]) for person in sorted(relations)]
     max_happiness = 0
-    for perm in tqdm(permutations(happiness_data.keys())):
-        happiness = 0
+    with tqdm() as pbar:
+        for perm in permutations(range(1, len(relations))):
+            perm = [0] + [*perm]
+            happiness = 0
 
-        for person1, person2 in zip(perm, perm[1:] + tuple([perm[0]])):
-            happiness += happiness_data[person1][person2]
-            happiness += happiness_data[person2][person1]
+            for index, person in enumerate(perm):
+                pbar.update()
+                if happiness + get_max_remaining(perm[index:], highests) < max_happiness:
+                    break
+                left = perm[index - 1]
+                right = perm[index + 1 if index + 1 < len(perm) else 0]
+                happiness += relations[person][left]
+                happiness += relations[person][right]
 
-        if happiness > max_happiness:
-            max_happiness = happiness
+            if happiness > max_happiness:
+                max_happiness = happiness
+
+        pbar.update()
 
     return max_happiness
 
