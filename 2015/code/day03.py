4316f148ec3177504414d45917f49f71fae988cf
Philémon van Helden <philemon.vanhelden@gmx.com>
2024-03-09 17:56:58 +0100

    [IMP] Day 03: minor performance improvement

--- 2015/code/day03.py
+++ 2015/code/day03.py
@@ -6,15 +6,14 @@ from utils import sum_tuples
 def find_final_positions(text: str):
     pos_part_1, pos_robot, pos_santa = [[0, 0]] * 3
     char_dic: dict[str, (int, int)] = {'^': (1, 0), '>': (0, 1), '<': (0, -1), 'v': (-1, 0)}
-    dic_part_1: dict[(int, int), int] = {(0, 0): 1}
-    dic_part_2: dict[(int, int), int] = {(0, 0): 1}
+    part_1 = {(0, 0)}
+    part_2 = {(0, 0)}
     for index, char in tqdm(enumerate(text)):
         move = char_dic[char]
 
         # Part 1
         pos_part_1 = sum_tuples(pos_part_1, move)
-        dic_part_1.setdefault(pos_part_1, 0)
-        dic_part_1[pos_part_1] += 1
+        part_1.add(pos_part_1)
 
         # Part 2
         if not index % 2:
@@ -23,9 +22,8 @@ def find_final_positions(text: str):
         else:
             pos_part_2 = sum_tuples(move, pos_robot)
             pos_robot = pos_part_2
-        dic_part_2.setdefault(pos_part_2, 0)
-        dic_part_2[pos_part_2] += 1
-    return len(dic_part_1.values()), len(dic_part_2.values())
+        part_2.add(pos_part_2)
+    return len(part_1), len(part_2)
 
 
 def main(filename: str):
