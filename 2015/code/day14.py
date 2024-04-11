6707a36c82f4b0e051f977af195faec6d462b3ce
Philémon van Helden <philemon.vanhelden@gmx.com>
2024-04-11 14:19:06 +0200

    [ADD] Day 14: part 2

--- 2015/code/day14.py
+++ 2015/code/day14.py
@@ -1,6 +1,13 @@
 import re
 
 
+def get_numbers(reindeers: [str]):
+    stats = []
+    for reindeer in reindeers:
+        stats.append([int(value) for value in re.findall(r'\d+', reindeer)])
+    return stats
+
+
 def find_total_distance(speed: int, endurance: int, rest: int, time: int):
     cycle = endurance + rest
     n_cycles = time // cycle
@@ -8,17 +15,36 @@ def find_total_distance(speed: int, endurance: int, rest: int, time: int):
     return speed * (endurance * n_cycles + extra_time)
 
 
-def find_best_distance(reindeers: [str], time=2503):
+def find_best_distance(stats: [int], time=2503):
     best = 0
-    for reindeer in reindeers:
-        values = [int(value) for value in re.findall(r'\d+', reindeer)]
-        distance = find_total_distance(*values, time)
+    index = -1
+    for i, reindeer in enumerate(stats):
+        distance = find_total_distance(*reindeer, time=time)
         if distance > best:
             best = distance
-    return best
+            index = i
+    return best, index
+
+
+def find_best_score(stats: [int], time=2503):
+    scores = [0] * len(stats)
+    for current in range(time):
+        _, index = find_best_distance(stats, time=current)
+        scores[index] += 1
+    return max(scores)
+
+
+def race_distance(lines: [str], time=2503):
+    stats = get_numbers(lines)
+    return find_best_distance(stats, time)[0]
+
+
+def race_score(lines: [str], time=2503):
+    stats = get_numbers(lines)
+    return find_best_score(stats, time)
 
 
 def main(filename: str):
     with open(filename) as file:
         lines = file.read().splitlines()
-        return find_best_distance(lines), 0
+        return race_distance(lines), race_score(lines)
